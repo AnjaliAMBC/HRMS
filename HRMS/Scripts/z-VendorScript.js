a86c6e496d942dae29e5162d73e6d38de04707f6
AnjaliAMBC <166894837+AnjaliAMBC@users.noreply.github.com>
2024-08-20 10:57:43 +0530

    IT support Vendor

--- HRMS/Scripts/z-VendorScript.js
+++ HRMS/Scripts/z-VendorScript.js
@@ -1,3 +1,6 @@
-﻿$(document).on('click', '.yourclassname', function () {
-    window.location.href = "/Vendor/index";
+﻿$(document).on('click', '.vendorBulk', function () {
+    window.location.href = "/Vendor/ImportVendor";
+});
+$(document).on('click', '.vendorManually', function () {
+    window.location.href = "/Vendor/AddVendor";
 });
