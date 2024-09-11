baaf929fa8ee91186a0bacf9de86dc932ccc6279
phenixcorecoder <prathikshak@ambconline.com>
2024-09-11 16:02:49 +0530

    -changes made in subscrition page for pop-up and some alignment issues.

--- HRMS/Scripts/z-SubscriptionScript.js
+++ HRMS/Scripts/z-SubscriptionScript.js
@@ -1,11 +1,8 @@
-﻿$(document).ready(function () {
-    $('#addsubscriptioninfo').change(function () {
-        if ($(this).val() === 'Addmanually') {
-            window.location.href = '/Subscription/AddSubscription';
-        }
-    });
-});
+﻿
 
 function redirectToSubscriptionHistory() {
     window.location.href = '/subscription/history';
+}
+function redirectToAddSubscription() {
+    window.location.href = '/Subscription/AddSubscription';
 }
