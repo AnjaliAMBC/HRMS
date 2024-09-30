72eef3159cf69d91250ab6d5b9d84dfbc0715f25
AnjaliAMBC <166894837+AnjaliAMBC@users.noreply.github.com>
2024-09-30 21:06:00 +0530

    Asset Functionality and Maintanance

---
+++ HRMS/Controllers/MaintananceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRMS.Controllers
+{
+    public class MaintananceController : Controller
+    {
+        // GET: Maintanance
+        public ActionResult MaintananceInfo()
+        {
+            return View("~/Views/Itsupport/Maintanance.cshtml");
+        }
+        public ActionResult EmpMaintananceHistory()
+        {
+            return View("~/Views/Itsupport/EmpMaintananceHistory.cshtml");
+        }
+        public ActionResult MaintananceApproval()
+        {
+            return View("~/Views/Itsupport/MaintananceApprovalView.cshtml");
+        }
+
+    }
+   }
--- HRMS/Scripts/z-AssetScript.js
+++ HRMS/Scripts/z-AssetScript.js
@@ -9,26 +9,55 @@ $(document).on('change', '#addassetinfo', function () {
 });
 
 
-$(document).on('click', '.Asset-id-select', function () {
-    window.location.href = '/Asset/AssetView';
+
+$(document).on('click', '.asset-info-view', function () {
+    var sno = $(this).attr("data-assetid");
+    window.location.href = '/asset/assetview?sno=' + sno;
+    return;
 });
 
-$(document).on('click', '.asset-info-edit', function () {
+
+$(document).on('click', '.Asset-id-select', function () {
+    var sno = $(this).attr("data-assetid");
+    window.location.href = '/asset/assetview?sno=' + sno;
+    return;
+});
+
+$(document).on('click', '.asset-info-edit', function (event) {
+    event.preventDefault();
     var sno = $(this).attr("data-assetid");
     window.location.href = '/asset/addasset?sno=' + sno;
 });
 
+function FormattedTransferDate(dateString) {
+    var date = new Date(dateString);
+    var year = date.getFullYear();
+    var month = ('0' + (date.getMonth() + 1)).slice(-2);
+    var day = ('0' + date.getDate()).slice(-2);
+    var formattedDate = year + '-' + month + '-' + day;
+    return formattedDate;
+}
 
-$(document).on('click', '.asset-info-transfer', function () {
+
+$(document).on('click', '.asset-info-transfer', function (event) {
+    event.preventDefault();
+    $('.isassettransferred').text("");
     var sno = $(this).attr("data-assetid");
     $('#assettransfer-popup').modal('show');
     $.ajax({
-        url: 'asset/assettransfer',
+        url: '/asset/assettransfer',
         type: 'GET',
         data: { sno: sno },
-        success: function (data) {
-            $('#AssetpopupContent').html(data);
-            $('#Assettransferpopup').show();
+        success: function (response) {
+            data = $.parseJSON(response);
+
+            $('.asset-transer-asseid').text(data.EditAssets.AssetID);
+            $('.asseettrasfer-assign-name').text(data.AllocatedEmpInfo.EmployeeName);
+            $('.asseettrasfer-assign-designation').text(data.AllocatedEmpInfo.Designation);
+            $('.asseettrasfer-assign-location').text(data.EditAssets.Location);
+            $('.asseettrasfer-assign-olddate').text(FormattedTransferDate(data.EditAssets.AssignedDate));
+            $('.asseettrasfer-assign-oldassignby').text(data.EditAssets.AssignedBy);
+            $('.asset-transer-assesno').text(data.EditAssets.SNo);
         },
         error: function (xhr, status, error) {
             alert("An error occurred: " + error);
@@ -37,7 +66,206 @@ $(document).on('click', '.asset-info-transfer', function () {
 });
 
 
-$(document).on('click', '#AssetclosePopup').click(function () {
-    $('#Assettransferpopup').hide();
+$(document).on('click', '.assettransfer-purchaseinfo-Cancel', function (event) {
+    event.preventDefault();
+    if ($('.isassettransferred').text() == "yes") {
+        window.location.href = "/asset/assetinfo";
+        return;
+    }
+
+    $('#assettransfer-popup').modal('hide');
 });
+
+
+$(document).on('click', '.assettransfer-Transfer', function (event) {
+    $('.isassettransferred').text("");
+    event.preventDefault();
+    var recordsno = $('.asset-transer-assesno').text();
+
+    $('.form-control').removeClass('is-invalid');
+    var isValid = true;
+
+    var locationName = $('#AssetTransfer-Location').val();
+    if (!locationName) {
+        $('#AssetTransfer-Location').addClass('is-invalid');
+        isValid = false;
+    }
+    else {
+        $('#AssetTransfer-Location').removeClass('is-invalid');
+    }
+
+    var employeeId = $('#AssetTransfer-Employee').val();
+    var employeeName = $('#AssetTransfer-Employee option:selected').text();
+    if (!employeeId) {
+        $('#AssetTransfer-Employee').addClass('is-invalid');
+        isValid = false;
+    }
+    else {
+        $('#AssetTransfer-Employee').removeClass('is-invalid');
+    }
+
+    var assignedById = $('#AssetTransfer-AssignedBy').val();
+    var assignedByName = $('#AssetTransfer-AssignedBy option:selected').text();
+    if (!assignedById) {
+        $('#AssetTransfer-AssignedBy').addClass('is-invalid');
+        isValid = false;
+    }
+    else {
+        $('#AssetTransfer-AssignedBy').removeClass('is-invalid');
+    }
+
+    var transferDate = $('#AssetTransfer-TransferDate').val();
+    if (!transferDate) {
+        $('#AssetTransfer-TransferDate').addClass('is-invalid');
+        isValid = false;
+    } else {
+        $('#AssetTransfer-TransferDate').removeClass('is-invalid');
+    }
+
+    if (isValid) {
+        var assetTransferPostModel = {
+            allocatedempid: employeeId,
+            allocatedempname: employeeName,
+            assignedbyid: assignedById,
+            assignedbyname: assignedByName,
+            transferdate: transferDate,
+            sno: recordsno,
+            location: $('#AssetTransfer-Location').val()
+        };
+
+        $.ajax({
+            url: '/asset/assettransfersubmit',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(assetTransferPostModel),
+            success: function (response) {
+                if (response.StatusCode == 200) {
+                    $('.isassettransferred').text("yes");
+                    $('#assettransfer-message').html(`<div class="alert alert-success" style="color: green;">Asset transfer successful!</div>`);
+                } else {
+                    $('.isassettransferred').text("");
+                    $('#assettransfer-message').html(`<div class="alert alert-danger" style="color: red;">Asset transfer failed! Please try again.</div>`);
+                }
+            },
+            error: function (xhr, status, error) {
+                $('.isassettransferred').text("");
+                $('#assettransfer-message').html(`<div class="alert alert-danger" style="color: red;">An error occurred: ${error}</div>`);
+            }
+        });
+    }
+    else {
+        $('#assettransfer-message').html(`<div class="alert alert-danger" style="color: red;">Please fill all required fields.</div>`);
+    }
+});
+
+
+$(document).on('click', '.asset-info-delete', function () {
+    var assetId = $(this).attr('data-assetid');
+    var assetName = $(this).attr('data-assetname');
+    $('#isAssetDeleted').text("");
+
+    $('#assetNameToDelete').text(assetName);
+    $('#deleteAssetModal').modal('show');
+
+    $('#confirmDeleteAsset').off('click').on('click', function () {
+        $.ajax({
+            url: '/asset/deleteasset',
+            type: 'POST',
+            data: { sno: assetId },
+            success: function (response) {
+                if (response.StatusCode === 200) {
+                    $('#deleteMessage').removeClass('alert-danger').addClass('alert-success');
+                    $('#deleteMessage').html('Asset deleted successfully!').show();
+                    $('#isAssetDeleted').text("yes");
+                } else {
+                    $('#deleteMessage').removeClass('alert-success').addClass('alert-danger');
+                    $('#deleteMessage').html('Failed to delete asset! Please try again.').show();
+                    $('#isAssetDeleted').text("");
+                }
+            },
+            error: function (xhr, status, error) {
+                $('#deleteMessage').removeClass('alert-success').addClass('alert-danger');
+                $('#deleteMessage').html('An error occurred: ' + error).show();
+                $('#isAssetDeleted').text("");
+            }
+        });
+    });
+});
+
+$(document).on('click', '.asset-delete-refresh', function (event) {
+    event.preventDefault();
+    if ($('#isAssetDeleted').text() == "yes") {
+        window.location.href = "/asset/assetinfo";
+        return;
+    }
+
+    $('#deleteAssetModal').modal('hide');
+});
+
+
+
+$('.btn-asset-import-submit').click(function () {
+    const fileInput = document.getElementById('asset-file-upload-input');
+    const file = fileInput.files[0];
+
+    if (!file) {
+        alert("Please select a file before submitting.");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    $.ajax({
+        url: '/asset/importassets',
+        type: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            if (response.StatusCode == 200) {
+                $('#assetImportSuccessModal').modal('show');
+                $('.asset-success-message').text(response.Message);
+                $('.asset-success-message').css('color', 'green');
+            } else {
+                $('#assetImportSuccessModal').modal('show');
+                $('.asset-success-message').text(response.Message);
+                $('.asset-success-message').css('color', 'red');
+            }
+        },
+        error: function (xhr, status, error) {
+            $('#assetImportSuccessModal').modal('show');
+            $('.asset-success-message').text(response.Message);
+            $('.asset-success-message').css('color', 'red');
+        }
+    });
+});
+
+
+$(document).on("click", ".btn-asset-refreshpage", function (event) {
+    event.preventDefault();
+    window.location.href = "/asset/assetinfo";
+    return;
+});
+
+
+function handleAssetFileUpload(input) {
+    const file = input.files[0];
+    if (file) {
+        document.getElementById('asset-uploaded-file-info').style.display = 'block';
+        document.getElementById('asset-uploaded-file-text').innerText = 'Selected file: ' + file.name;
+    }
+}
+
+
+
+function downloadAssetTemplate() {
+    var link = document.createElement('a');
+    link.href = '/assets/templates/Sample Assets Import.xlsx';
+    link.download = 'AssetImportTemplate.xlsx';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
 
