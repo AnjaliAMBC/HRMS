e79e4cbb079cfa568eb0e420fc8025df948f43e9
AnjaliAMBC <166894837+AnjaliAMBC@users.noreply.github.com>
2024-11-12 11:33:21 +0530

    Table change

---
+++ HRMS/Models/Employee/EmpJobModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRMS.Models.Employee
+{
+    public class EmpJobModel
+    {
+        public List<JobDetail> jobdetail  = new List<JobDetail>();
+    }
+}
