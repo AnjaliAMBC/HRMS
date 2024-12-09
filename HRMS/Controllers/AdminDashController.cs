4dfd243ca29abc22c9256f3f3022d9bce8818b79
AnjaliAMBC <166894837+AnjaliAMBC@users.noreply.github.com>
2024-12-09 20:43:08 +0530

    - Holiday import draft code

--- HRMS/Controllers/AdminDashController.cs
+++ HRMS/Controllers/AdminDashController.cs
@@ -1,5 +1,6 @@
 ﻿using HRMS.Helpers;
 using HRMS.Models.Admin;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -7,7 +8,8 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-
+using System.IO;
+using System.Data;
 
 namespace HRMS.Controllers
 {
@@ -67,12 +69,122 @@ namespace HRMS.Controllers
 
         public ActionResult AdminHolidayView()
         {
-            return View("~/Views/AdminDashboard/AdminHolidayListView.cshtml");
+            var currentYear = DateTime.Today.Year;
+            var holidaysList = _dbContext.tblambcholidays
+                                         .Where(x => x.holiday_date.HasValue && x.holiday_date.Value.Year == currentYear)
+                                         .ToList(); // Fetch the list
+
+            // Pass the holidays list to the view
+            return View("~/Views/AdminDashboard/AdminHolidayListView.cshtml", holidaysList);
         }
+
+
+
         public ActionResult HolidayImport()
         {
             return View("~/Views/AdminDashboard/HolidayBulkImport.cshtml");
         }
+
+
+        [HttpPost]
+        public ActionResult ImportHolidays(HttpPostedFileBase file)
+        {
+            var model = new ImportEmployeeViewModel();
+            try
+            {
+                // Check if a file is uploaded
+                if (file != null && file.ContentLength > 0)
+                {
+                    List<tblambcholiday> holidayList = new List<tblambcholiday>();
+
+                    using (ExcelPackage package = new ExcelPackage(file.InputStream))
+                    {
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                        DataTable dt = new DataTable();
+
+                        int colCount = worksheet.Dimension.End.Column;
+                        for (int col = 1; col <= colCount; col++)
+                        {
+                            dt.Columns.Add(worksheet.Cells[1, col].Value.ToString().Trim());
+                        }
+
+                        int rowCount = worksheet.Dimension.End.Row;
+                        for (int row = 2; row <= rowCount; row++)
+                        {
+                            bool isEmptyRow = true;
+                            for (int col = 1; col <= colCount; col++)
+                            {
+                                if (worksheet.Cells[row, col].Value != null)
+                                {
+                                    isEmptyRow = false;
+                                    break;
+                                }
+                            }
+
+                            if (isEmptyRow)
+                            {
+                                continue;
+                            }
+
+                            DataRow dataRow = dt.NewRow();
+                            for (int col = 1; col <= colCount; col++)
+                            {
+                                dataRow[col - 1] = worksheet.Cells[row, col].Value;
+                            }
+
+                            dt.Rows.Add(dataRow);
+                            tblambcholiday holidayInfo = new tblambcholiday();
+
+                            holidayInfo.holiday_date = System.Convert.ToDateTime(dataRow["Holiday Date"].ToString());
+                            holidayInfo.holiday_name = dataRow["Holiday Name"].ToString();
+                            holidayInfo.region = dataRow["Region"].ToString();
+
+                            holidayList.Add(holidayInfo);
+                        }
+                    }
+
+                    if (holidayList.Count > 0)
+                    {
+                        _dbContext.tblambcholidays.AddRange(holidayList);
+                        _dbContext.SaveChanges();
+
+
+                    }
+                }
+
+                return Json(new { success = true, message = "Holidays list import is successful!" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Holidays list import is un-successful!" });
+
+            }
+        }
+
+        public ActionResult UpdateHoliday(int holidayId, string holidayName, string holidayDate)
+        {
+            var model = new ImportEmployeeViewModel();
+            try
+            {
+                var holiday = _dbContext.tblambcholidays.Where(x => x.holidayno == holidayId).FirstOrDefault();
+
+                if (holiday != null)
+                {
+                    holiday.holiday_name = holidayName;
+                    holiday.holiday_date = System.DateTime.Parse(holidayDate);
+                    _dbContext.SaveChanges();
+                }
+
+                return Json(new { success = true, message = "Holiday updated successfully!" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Holidays update un-successful!" });
+
+            }
+        }
+
+
         public ActionResult JobImport()
         {
             return View("/Views/AdminDashboard/JobBulkImport.cshtml");
